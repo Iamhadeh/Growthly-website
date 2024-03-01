c52dafab83d39e0fe4677d9a3421ea7734607db4
Adekusibe Ogunmokun <147047093+Iamhadeh@users.noreply.github.com>
2024-03-01 20:53:58 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
 
 
+
+// The funtion for dropdown menu 
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
+
+
+// This part
 var mainHeaderBox = document.querySelector (".main-header-box"); 
 
 mainHeaderBox.addEventListener ("scroll", function(){
